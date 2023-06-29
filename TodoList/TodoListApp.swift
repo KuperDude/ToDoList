d49c7ecbe0eb3d903a24031aa60af48c3266e1df
KuperDude <fieldsdev06@gmail.com>
2023-06-29 18:19:07 +0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,24 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by MyBook on 29.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ListView()
+            }
+            .navigationViewStyle(.stack)
+            .environmentObject(listViewModel)
+        }
+    }
+}
